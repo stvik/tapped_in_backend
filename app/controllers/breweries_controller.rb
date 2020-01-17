038aaf40df352ab3c98272ee9aae146ed2738daf
stvik <stvik2@gmail.com>
2020-01-17 13:00:13 -0500

    able to pull data directly

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,8 +1,20 @@
 class BreweriesController < ApplicationController
 
 	def index
-        render json: Brewery.all
+
+		Brewery.destroy_all
+
+		if params[:state]
+			Brewery.get_breweries_data('state',params[:state])
+		elsif params[:name]
+			Brewery.get_breweries_data('name',params[:name])
+		else
+			Brewery.get_breweries_data('state','District of Columbia')
+		end
+
+        render json: Brewery.all.sort_by{|brewery| brewery[:name]}
     end
+
 	
 
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,34 @@
 class Brewery < ApplicationRecord
 	has_many :user_brews
 	has_many :users, through: :user_brews
+
+
+	def self.get_breweries_data(parameter, state)
+
+		brews = RestClient.get("https://api.openbrewerydb.org/breweries?by_#{parameter}=#{state}&per_page=50")
+
+		brews_array = JSON.parse(brews)
+
+
+		brews_array.each do |brew|
+			if brew['brewery_type'] != 'planning'
+				picture = ('https://source.unsplash.com/random/900%C3%97700/?brewery')
+			    Brewery.create(
+			        name: brew['name'],
+			        street: brew['street'],
+			        city: brew['city'],
+			        state: brew['state'],
+			        postal_code: brew['postal_code'],
+			        longitude: brew['longitude'].to_f,
+			        latitude: brew['latitude'].to_f,
+			        website_url: brew['website_url'],
+			        rating: 5,
+			        description: Faker::Lorem.paragraph(sentence_count: 5),
+			        image: picture
+			    )
+			end
+		end
+	end
+
+
 end
